71ded6f9d21c456b022a12f0057d38aa26a5bf75
Mickey Weiss <mickeyweiss@Mickeys-MacBook-Pro.local>
2024-08-21 20:05:11 -0600

    regular commit

--- server/index.js
+++ server/index.js
@@ -1,1 +1,14 @@
-const {client} = require('./db')
+const {client, createTables} = require('./db');
+const express = require ('express');
+const app = express();
+app.use (express.json());
+
+const init = async () => {
+    await client.connect();
+    console.log('connected to the database');
+    await createTables();
+    console.log('tables created');
+    
+}
+
+init();
